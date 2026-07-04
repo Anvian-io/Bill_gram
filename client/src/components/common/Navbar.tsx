cf9498d8e071cd9cb1ebd1a5fef1b95379d8d805
suthakaranburaj <suthakaranburaj@gmail.com>
2026-07-04 15:24:14 +0530

    version updated to 1.1.0

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -140,7 +140,7 @@ export function Navbar({ children, isExpanded, setIsExpanded }: NavbarProps) {
                     isExpanded ? "opacity-100" : "opacity-0"
                   }`}
                 >
-                  Dashboard v1.0.5
+                  Dashboard v1.1.0
                 </span>
               </div>
             </div>
